da8ec0bc058dd01d43a0a72d254609384140ee1a
Salman Mahmud <salman_mahmud@hotmail.com>
2022-10-30 14:57:27 -0400

    commit tree a10801b63f26bce906da28eaf77d6cc61a7075b9

---
+++ app.rb
@@ -0,0 +1,36 @@
+# app.rb
+# puts 'Hello World!'
+# puts("Hello World!")
+
+#print does not add a break line at the end of the string
+
+# print "Hello World"
+# print("Hello World")
+
+#these are better for inspecting data while the previous two methods are great for working with 
+#strings and displaying them in the terminal
+#this would be better for things like arrays
+
+# p "Hello World"
+# p("Hello World")
+# pp "Hello World"
+# pp("Hello World")
+
+# puts [1,2,3]
+# print [3,4,5]
+# p [6,7,8]
+# pp [9,10,11]
+
+
+# pp [{id:1, hello: "World" }, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+#print [{id:1, hello: "World" }, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+#puts [{id:1, hello: "World" }, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+#p [{id:1, hello: "World" }, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
